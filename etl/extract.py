8eb0e8cdd52d2b4eb43b9f74a479fc7d70a53637
siharp <siharpangaribuan03@gmail.com>
2025-04-28 21:15:45 +0700

    update code extract data

--- etl/extract.py
+++ etl/extract.py
@@ -2,33 +2,47 @@ import os
 import json
 import logging
 from datetime import datetime
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from bson import json_util
 
 def extract_data(uri:str, db_name:str, collection_name:str, result_dir:str, start_date:int, end_date:int) -> str:
     logging.info('Start extracting data from MongoDB')
-    client = None
+    result_path = None
+
     try:
-        client = MongoClient(uri)
-        collection = client[db_name][collection_name]
-        logging.info('Connect to database')
-        query = {"db_created_at": {"$gte": start_date, "$lt": end_date}}
+        with MongoClient(uri) as  client:
+            client.server_info()
+            logging.info('Connected to MongoDB')
 
-        logging.info(f'Quering data from {collection_name}')
-        data = list(collection.find(query).batch_size(5000))
+            db = client[db_name]
+            collection = db[collection_name]
+
+            query = {"db_created_at": {"$gte": start_date, "$lt": end_date}}
+            logging.info(f'Querying data from collection: {collection_name}')
+            data = list(collection.find(query).batch_size(5000))
+
+            if not data:
+                logging.warning('No data found for given date range.')
+
+            os.makedirs(result_dir, exist_ok=True)
+            result_path = os.path.join(result_dir, f'{collection_name}_{start_date}.json')
+
+            with open(result_path, 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=4, default=json_util.default)
+            logging.info(f'Data successfully extracted dan savd to {result_path}')
+            return result_path
         
-        os.makedirs(result_dir, exist_ok=True)
-        today = datetime.now().strftime('%Y-%m-%d')
-        result_path = os.path.join(result_dir, f'{collection_name}_{today}.json')
+    except errors.ServerSelectionTimeoutError as conn_err:
+        logging.error(f'Failed to connect to MongoDB server: {conn_err}')
 
-        with open(result_path, 'w', encoding='utf-8') as f:
-            json.dump(data, f, indent=4, default=json_util.default)
-        return result_path
+    except errors.PyMongoError as mongo_err:
+        logging.error(f'MongoDB operation error: {mongo_err}')
+
+    except (OSError, IOError) as file_err:
+        logging.error(f'File operation error: {file_err}')
 
     except Exception as e:
-        logging.error('Error Conect to database')
+        logging.error(f'Unexpected error during extraction: {e}')
 
-    finally:
-        logging.info(f'Extracted data saved to {result_path}')
-        client.close()
+    return result_path
        
