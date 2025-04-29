d677e60eb9b6eb4882f638a11183ccf6b13d5707
siharpangaribuan <siharpangaribuan14@gmail.com>
2025-04-29 11:40:19 +0700

    update naming clear to clean

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from datetime import datetime, timezone, timedelta
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == '__main__':
-    
+
     raw_path = extract_data(MONGO_URI, MONGO_DB, MONGO_COLLECTION, RAW_DIR, START_DATE, END_DATE)
 
-    clear_path = transform_data(raw_path, MONGO_COLLECTION, CLEAR_DIR, FILTER_DUP)
+    clear_path = transform_data(raw_path, MONGO_COLLECTION, CLEAN_DIR, FILTER_DUP)
 
     load_data(clear_path, PG_HOST, PG_DB, PG_PORT, PG_USER, PG_PASSWORD, MONGO_COLLECTION)
